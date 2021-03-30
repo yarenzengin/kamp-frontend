654ecd2724266a636467328a0b4698ec9b6d96ec
Yaren <avote841@gmail.com>
2021-03-30 15:54:27 +0300

    listeleme geliştirildi

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+categories:Category[]=[];
+currentCategory:Category={categoryId:0, categoryName:""};
+  constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
   }
-
+getCategories(){
+  this.categoryService.getCategories().subscribe((response)=>{
+  this.categories = response.data
+  } )
+}
+setCurrentCategory(category:Category){
+  this.currentCategory = category;//çekmiş olduk
+}
+getCurrentCategoryClass(category:Category){
+  if (category == this.currentCategory ) {
+    return "list-group-item active"
+  }else{
+    return "list-group-item"
+  }
+ 
+}
+getAllCategory(){
+  if (!this.currentCategory ) {
+    return "list-group-item active"
+  }else{
+    return "list-group-item"
+  }
+ 
+}
 }
--- src/app/models/categoryResponceModel.ts
+++
@@ -1,7 +0,0 @@
-import { Category } from "./category";
-import { ResponseModel } from "./responseModel";
-
-export interface CategoryResponceModel extends ResponseModel{
-    data:Category[]
-    
-}
